cce6d0c5b853e09d0a342680574dbce411d7c11e
Hawks Jamesf <hawks.jamesf@gmail.com>
2020-07-07 15:32:11 +0800

    add pythondoc

--- app/src/main/java/com/hawksjamesf/designpatterns/creational/python/singleton.py
+++ app/src/main/java/com/hawksjamesf/designpatterns/creational/python/singleton.py
@@ -1,3 +1,13 @@
+# -*- coding: utf-8 -*-
+"""元编程：
+ type 本身就是元类
+1. 元类
+2. 装饰器：函数装饰器、类装饰器
+
+__new__ vs. __init__:当需要控制新实例的创建时使用 new，而在需要控制新实例的初始化时则使用 init
+
+
+ """
 class Singleton(type):
     """
     Singleton Metaclass
@@ -10,5 +20,7 @@ class Singleton(type):
             cls._inst[cls] = super(Singleton, cls).__call__(*args)
         return cls._inst[cls]
 
-class PythonSingleton(metaclass = Singleton):
+class PythonSingleton():
+    __metaclass__ = Singleton
     pass
+print(type(PythonSingleton))
